7b66831e75a12133a05f9de27f9b52e9a8361d49
uzumakinoharu <59409791+uzumaki-noharu@users.noreply.github.com>
2022-02-27 19:39:43 +0800

    Create 450. 删除二叉搜索树中的节点（中等）.c

---
+++ 题目/450. 删除二叉搜索树中的节点（中等）.c
@@ -0,0 +1,22 @@
+//主要问题在与递归关系没有搞清楚
+//递归中的下一级的return不仅可以作为一个变量存储信息返回到最初的递归函数中，还可以作为上一级的root改变自身的属性的变量。
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) 
+    {
+        if (root == nullptr)    return nullptr;
+        if (key > root->val)    root->right = deleteNode(root->right, key);     // 去右子树删除
+        else if (key < root->val)    root->left = deleteNode(root->left, key);  // 去左子树删除
+        else    // 当前节点就是要删除的节点
+        {
+            if (! root->left)   return root->right; // 情况1，欲删除节点无左子
+            if (! root->right)  return root->left;  // 情况2，欲删除节点无右子
+            TreeNode* node = root->right;           // 情况3，欲删除节点左右子都有 
+            while (node->left)          // 寻找欲删除节点右子树的最左节点
+                node = node->left;
+            node->left = root->left;    // 将欲删除节点的左子树成为其右子树的最左节点的左子树
+            root = root->right;         // 欲删除节点的右子顶替其位置，节点被删除
+        }
+        return root;    
+    }
+};
